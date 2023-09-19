9d7793e1b675635683673e7653a4ab4389307b48
Infinix media <122368594+InfinixMediaDev@users.noreply.github.com>
2023-09-19 23:12:47 +0700

    Change base purge-cache.php
    Changing Purge Cache Plugins, Show Monthly bandwidth usage for the zone and add css for styling

--- purge-cache.php
+++ purge-cache.php
@@ -1,144 +1,116 @@
 <?php
 /*
-Plugin Name: Purge Cache
-Description: A plugin that allows you to purge the cache of a specific CDN pull zone.
+Plugin Name: Purge Cache Bunny CDN
+Description: A custom WordPress plugin for purging Bunny CDN cache and displaying usage information.
+Version: 1.0
+Author: Your Name
 */
 
-// Register the administration menu option
-add_action('admin_menu', 'purge_cache_menu');
-function purge_cache_menu() {
-    add_options_page('Purge CDN Cache', 'Purge CDN Cache', 'manage_options', 'purge-cache', 'purge_cache_options');
+// Define Bunny CDN API endpoint
+define('BUNNY_API_ENDPOINT', 'https://api.bunny.net/');
+
+// Hook into the WordPress admin menu
+add_action('admin_menu', 'purge_cache_bunny_cdn_menu');
+
+// Create a menu item for Purge Cache Bunny CDN in the WordPress admin
+function purge_cache_bunny_cdn_menu() {
+    add_menu_page('Purge Cache Bunny CDN', 'Purge Cache Bunny CDN', 'manage_options', 'purge-cache-bunny-cdn', 'purge_cache_bunny_cdn_page');
 }
 
-// Display the options page
-function purge_cache_options() {
-    if (!current_user_can('manage_options')) {
-        wp_die(__('You do not have sufficient permissions to access this page.'));
-    }
+// Callback function for the plugin page
+function purge_cache_bunny_cdn_page() {
     ?>
     <div class="wrap">
-        <h2>Purge CDN Cache</h2>
-        <form method="post" action="options.php">
-            <?php settings_fields('purge_cache_options'); ?>
-            <?php do_settings_sections('purge-cache'); ?>
-            <table class="form-table">
-                <tr valign="top">
-                    <th scope="row">Pull Zone ID</th>
-                    <td>
-                        <input type="text" name="pull_zone_id" value="<?php echo get_option('pull_zone_id'); ?>" />
-                    </td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">API Key</th>
-                    <td>
-                        <input type="text" name="api_key" value="<?php echo get_option('api_key'); ?>" />
-                    </td>
-                </tr>
-            </table>
-            <?php submit_button(); ?>
+        <h2>Purge Cache Bunny CDN</h2>
+        
+        <!-- Purge Cache Form -->
+        <form method="post" action="">
+            <label for="zone_id">Zone ID:</label>
+            <input type="text" name="zone_id" id="zone_id" required />
+            <input type="submit" name="purge_cache" class="button-primary" value="Purge Cache" />
         </form>
+        
+        <?php
+        if (isset($_POST['purge_cache'])) {
+            $zone_id = sanitize_text_field($_POST['zone_id']);
+            $api_key = get_option('bunny_cdn_api_key');
+            
+            // Check if API key is set
+            if (empty($api_key)) {
+                echo '<div class="error"><p>API key is not set. Please configure it on the settings page.</p></div>';
+            } else {
+                $result = purge_bunny_cdn_cache($api_key, $zone_id);
+                if ($result === true) {
+                    echo '<div class="updated"><p>Cache purge request sent successfully.</p></div>';
+                } else {
+                    echo '<div class="error"><p>Cache purge request failed: ' . esc_html($result) . '</p></div>';
+                }
+            }
+        }
+        ?>
+        
+        <!-- Display Usage Information -->
+        <?php
+        $api_key = get_option('bunny_cdn_api_key');
+        if (!empty($api_key)) {
+            $bandwidth_usage = get_monthly_bandwidth_usage($api_key);
+            echo '<p>' . esc_html($bandwidth_usage) . '</p>';
+        }
+        ?>
     </div>
     <?php
 }
 
-// Register the plugin's options
-add_action('admin_init', 'purge_cache_admin_init');
-function purge_cache_admin_init() {
-    register_setting('purge_cache_options', 'pull_zone_id');
-    register_setting('purge_cache_options', 'api_key');
-}
+// Function to purge cache using Bunny CDN API
+function purge_bunny_cdn_cache($api_key, $zone_id) {
+    $url = BUNNY_API_ENDPOINT . '/pullzone/' . $zone_id . '/purgeCache?apikey=' . $api_key;
 
-// Function to purge the cache
-function purge_cache() {
-    // Get the pull zone ID and API key
-    $pull_zone_id = get_option('pull_zone_id');
-    $api_key = get_option('api_key');
+    // Make the API request
+    $response = wp_remote_post($url);
 
-    // Build the API endpoint URL
-    $api_url = "https://api.bunny.net/pullzone/$pull_zone_id/purgeCache";
+    if (is_wp_error($response)) {
+        return 'Error sending cache purge request.';
+    }
 
-    // Build the headers for the API request
-    $headers = array(
-        "AccessKey: $api_key",
-        "Content-Type: application/json"
-    );
-
-    // Build the options for the API request
-    $options = array(
-        'http' => array(
-            'header'  => $headers,
-            'method'  => 'POST',
-        ),
-    );
-    $context  = stream_context_create($options);
-
-    // Send the API request to purge
-    $result = file_get_contents($api_url, false, $context);
-
-    // Check if the request was successful
-    if ($result === false) {
-        $error = error_get_last();
-        wp_die("An error occurred while purging the cache: " . $error['message']);
+    $code = wp_remote_retrieve_response_code($response);
+    if ($code === 200) {
+        return true;
     } else {
-        add_action( 'admin_notices', 'purge_cache_success_notice' );
+        return 'Cache purge request failed with status code ' . $code;
     }
 }
 
-// Display a success notice after the cache has been purged
-function purge_cache_success_notice() {
-    ?>
-    <div class="notice notice-success is-dismissible">
-        <p>The cache has been successfully purged.</p>
-    </div>
-    <?php
+// Function to get monthly bandwidth usage
+function get_monthly_bandwidth_usage($api_key) {
+    $url = BUNNY_API_ENDPOINT . '/statistics/bandwidth?apikey=' . $api_key;
+
+    // Make the API request
+    $response = wp_remote_get($url);
+
+    if (is_wp_error($response)) {
+        return 'Error fetching data from Bunny CDN API.';
+    }
+
+    $body = wp_remote_retrieve_body($response);
+    $data = json_decode($body, true);
+
+    if (is_array($data) && isset($data['value'])) {
+        return 'Monthly Bandwidth Usage: ' . $data['value'] . ' GB';
+    } else {
+        return 'Unable to retrieve bandwidth usage data.';
+    }
 }
 
-// Add the "Purge Cache" button to the admin bar
-add_action( 'wp_before_admin_bar_render', 'purge_cache_admin_bar_button' );
-function purge_cache_admin_bar_button() {
-    global $wp_admin_bar;
-
-    $wp_admin_bar->add_node( array(
-        'id' => 'purge-cache',
-        'title' => 'Purge CDN Cache',
-        'href' => '#',
-        'meta' => array( 'onclick' => 'purgeCache()' )
-    ) );
+// Add "Purge Cache" button to admin bar
+function add_purge_cache_button_to_admin_bar() {
+    if (current_user_can('manage_options')) {
+        global $wp_admin_bar;
+        $wp_admin_bar->add_node(array(
+            'id' => 'purge-cache-bunny-cdn',
+            'title' => 'Purge Cache',
+            'href' => admin_url('admin.php?page=purge-cache-bunny-cdn'),
+        ));
+    }
 }
-
-// Add the JavaScript function to handle the button click
-add_action( 'admin_footer', 'purge_cache_javascript' );
-function purge_cache_javascript() {
-    ?>
-    <script>
-        function purgeCache() {
-            // Get the pull zone ID and API key
-            var pull_zone_id = '<?php echo get_option( 'pull_zone_id' ); ?>';
-            var api_key = '<?php echo get_option( 'api_key' ); ?>';
-
-            // Build the API endpoint URL
-            var api_url = "https://api.bunny.net/pullzone/" + pull_zone_id + "/purgeCache";
-
-            // Build the headers for the API request
-            var headers = {
-                "AccessKey": api_key,
-                "Content-Type": "application/json"
-            };
-
-            // Send the API request to purge the cache
-            fetch(api_url, {
-                method: 'POST',
-                headers: headers
-            })
-            .then(response => {
-                if (response.ok) {
-                    alert('Cache purged successfully!');
-                } else {
-                    alert('An error occurred while purging the cache: ' + response.statusText);
-                }
-            })
-            .catch(error => alert('An error occurred while purging the cache: ' + error));
-        }
-    </script>
-    <?php
-}
+add_action('admin_bar_menu', 'add_purge_cache_button_to_admin_bar', 999);
+?>
